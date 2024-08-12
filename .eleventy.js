20a145b38695fc26218ef36758703150b07b7d67
jckuri <jckuri@github.com>
2024-08-12 03:22:29 -0500

    Updated deploy.yaml

--- .eleventy.js
+++ .eleventy.js
@@ -66,7 +66,8 @@ module.exports = config => {
   // as the site URL will be https://<username>.github.io/<repo>/index.html
   // when deploying to Pages, set the PATH_PREFIX environment variable to your 
   // repository name
-  const pathPrefix = process.env.PATH_PREFIX || '/';
+  const pathPrefix = process.env.PATH_PREFIX || '/Automating_a_Simple_CI_CD_Pipeline/';
+  // const pathPrefix = process.env.PATH_PREFIX || '/';
 
   return {
     markdownTemplateEngine: 'njk',
